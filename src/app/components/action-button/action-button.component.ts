d26229754142152f715a56fe85cbe05601c7cf33
Jonathan Ironman <jona84xl@gmail.com>
2020-01-14 17:02:01 +0100

    Made button general purpose

--- src/app/components/action-button/action-button.component.ts
+++ src/app/components/action-button/action-button.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState } from 'src/app/app.store';
-import { ACTION_ADVENTURE } from '../../store/actions/actions';
-import { GameService } from '../../services/game.service';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-action-button',
@@ -10,10 +6,6 @@ import { GameService } from '../../services/game.service';
   styleUrls: ['./action-button.component.scss']
 })
 export class ActionButtonComponent implements OnInit {
-  adventureTime() {
-    this.gameService.adventure();
-  }
-
-  constructor(private gameService: GameService) {}
-  ngOnInit() {}
+  constructor() { }
+  ngOnInit() { }
 }
