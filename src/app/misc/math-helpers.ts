13926f0142f1396f598621c969c49536ad253e9f
Jonathan Ironman <jona84xl@gmail.com>
2020-01-21 22:36:43 +0100

    RandomInt now also returns int for decimal input

--- src/app/misc/math-helpers.ts
+++ src/app/misc/math-helpers.ts
@@ -1,6 +1,6 @@
 export class MathHelpers {
     static getRandomInt(min: number, max: number): number {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.floor(Math.random() * (max - min + 1)) + Math.round(min);
     }
 
     static chance(percentage: number): boolean {
