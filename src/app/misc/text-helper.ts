9359a631aebf5ece0ed4dbfcf47b2a6cc71a7b6a
Jonathan Ironman <jona84xl@gmail.com>
2020-02-07 23:55:39 +0100

    Lists now handles single subject correctly

--- src/app/misc/text-helper.ts
+++ src/app/misc/text-helper.ts
@@ -1,15 +1,24 @@
 export class TextHelpers {
-    static singularPlural(subject: [], singular: string, plural: string): string {
+    static singularPlural(subject: any[], singular: string, plural: string): string {
         return subject.length > 1 && plural || singular;
     }
+
     static listAnd(list: string[]): string {
+        if (list.length === 1) {
+            return list[0];
+        }
         const last = list.pop();
         return list.join(', ') + ' and ' + last;
     }
+
     static listOr(list: string[]): string {
+        if (list.length === 1) {
+            return list[0];
+        }
         const last = list.pop();
         return list.join(', ') + ' or ' + last;
     }
+
     static list(list: string[]): string {
         return list.join(', ');
     }
